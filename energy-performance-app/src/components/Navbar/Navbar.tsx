173e4deb4b7bd6f56189bc244e5063050b781088
Benjamin Gregersen <126244327+CodeMaster633@users.noreply.github.com>
2025-06-30 13:20:09 +0200

    feat: navbar init

---
+++ energy-performance-app/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,14 @@
+// import React, { FC } from 'react';
+// import { HeroWrapper } from './Hero.styled';
+
+import type { FC } from "react";
+import './Navbar.css'
+ 
+
+const Navbar: FC = () => (
+  <div className="navbar">
+    <h2>Energy Performance</h2>
+  </div>
+);
+
+export default Navbar;
