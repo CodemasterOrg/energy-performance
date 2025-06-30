108945bf3d67796f5d73908f8e730342d3649def
Benjamin Gregersen <126244327+CodeMaster633@users.noreply.github.com>
2025-06-30 12:45:46 +0200

    feat: hero

--- energy-performance-app/src/components/Hero/Hero.tsx
+++ energy-performance-app/src/components/Hero/Hero.tsx
@@ -2,11 +2,31 @@
 // import { HeroWrapper } from './Hero.styled';
 
 import type { FC } from "react";
+import video from '../../assets/Video_Energy_Windmill.mp4'
+import './Hero.css'
+import { Typewriter } from 'react-simple-typewriter';
+ 
 
-interface HeroProps {}
-
-const Hero: FC<HeroProps> = () => (
- <h1>Hero Component</h1>
+const Hero: FC = () => (
+  <div className="hero">
+    <video src={video} autoPlay loop muted />
+    <div className="content">
+     <h1>
+         Energize your company with{' '}
+        <span style={{ color: '#ffffff' }}>
+          <Typewriter
+            words={['wind', 'solar', 'water', 'bio', 'wind']} 
+            loop={1} 
+            typeSpeed={100}
+            deleteSpeed={50}
+            delaySpeed={2500}
+            cursor
+            cursorStyle="|"
+          />
+        </span>
+      </h1>
+    </div>
+  </div>
 );
 
 export default Hero;
