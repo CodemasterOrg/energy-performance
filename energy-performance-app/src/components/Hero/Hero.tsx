39875b750d04044a04ef1c169f10c211a202a82b
Benjamin Gregersen <126244327+CodeMaster633@users.noreply.github.com>
2025-06-30 10:35:41 +0200

    feat: setup projekt

---
+++ energy-performance-app/src/components/Hero/Hero.tsx
@@ -0,0 +1,12 @@
+// import React, { FC } from 'react';
+// import { HeroWrapper } from './Hero.styled';
+
+import type { FC } from "react";
+
+interface HeroProps {}
+
+const Hero: FC<HeroProps> = () => (
+ <h1>Hero Component</h1>
+);
+
+export default Hero;
