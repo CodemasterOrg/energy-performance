1ab35e25c8081d9fae4255e73fb52995b726f4f4
Benjamin Gregersen <126244327+CodeMaster633@users.noreply.github.com>
2025-07-01 12:30:57 +0200

    feat: navbar transparent

--- energy-performance-app/src/components/Navbar/Navbar.tsx
+++ energy-performance-app/src/components/Navbar/Navbar.tsx
@@ -3,11 +3,18 @@
 
 import type { FC } from "react";
 import './Navbar.css'
- 
+ import { HiLightningBolt } from "react-icons/hi";
+
 
 const Navbar: FC = () => (
   <div className="navbar">
-    <h2>Energy Performance</h2>
+    <h2><HiLightningBolt size={18}/>
+Energy Performance</h2>
+    <ul className="navItems">
+      <li className="item">About</li>
+      <li className="item">My overview</li>
+
+    </ul>
   </div>
 );
 
