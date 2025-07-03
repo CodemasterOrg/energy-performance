c234e41c300c409b7a30ac775eb19b3b706fec07
Benjamin Gregersen <126244327+CodeMaster633@users.noreply.github.com>
2025-07-03 11:25:45 +0200

    feat: footer white

--- energy-performance-app/src/components/Footer/Footer.tsx
+++ energy-performance-app/src/components/Footer/Footer.tsx
@@ -6,8 +6,9 @@ import './Footer.css'
 
 const Footer: FC = () => (
   <div className="footer">
-    <h2><HiLightningBolt size={18}/>
+    <h2 className="title"><HiLightningBolt size={18} color="black"/>
 Energy Performance</h2>
+<p className="contact">Contact us now</p>
     <ul className="fnavItems">
       <li className="item">About</li>
       <li className="item">My overview</li>
