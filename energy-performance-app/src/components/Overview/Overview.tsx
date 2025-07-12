1b22f279698ed44d45bb60fc84ddf21016dd4a77
Benjamin Gregersen <126244327+CodeMaster633@users.noreply.github.com>
2025-07-12 20:36:13 +0200

    feat: table scaffold

--- energy-performance-app/src/components/Overview/Overview.tsx
+++ energy-performance-app/src/components/Overview/Overview.tsx
@@ -1,12 +1,15 @@
 import './Overview.css'
 import Footer from '../Footer/Footer'
 import Navbar from '../Navbar/Navbar'
+import Table from '../Table/Table'
 
 function Overview() {
   return (
     <>
     <Navbar/>
+    <Table/>
     <h1>TEST</h1>
+
     <Footer/>
     </>
   )
---
+++ energy-performance-app/src/components/Table/Table.tsx
@@ -0,0 +1,9 @@
+import type { FC } from "react";
+
+const Table: FC = () => (
+<>
+<h1>Table</h1>
+</>
+)
+
+export default Table
