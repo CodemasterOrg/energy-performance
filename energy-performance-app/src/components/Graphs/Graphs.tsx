88de1ee4fcc9696176ee21ca2d3b72f3cd1f0e22
Benjamin Gregersen <126244327+CodeMaster633@users.noreply.github.com>
2025-08-12 15:11:42 +0200

    feat:graphs css

--- energy-performance-app/src/components/Graphs/Graphs.tsx
+++ energy-performance-app/src/components/Graphs/Graphs.tsx
@@ -1,23 +1,27 @@
 import type { FC } from "react";
 import { BarChart } from "@mui/x-charts/BarChart";
+import "./Graphs.css";
 
 const Graphs: FC = () => (
   <>
-    <h1>Graphs</h1>;<h2>Energy production today</h2>
-    <BarChart
-      xAxis={[
-        {
-          id: "barCategories",
-          data: ["Solar", "Wind", "Water"],
-        },
-      ]}
-      series={[
-        {
-          data: [2, 5, 3],
-        },
-      ]}
-      height={300}
-    />
+    <h1>Graphs</h1>
+    <h2>Energy production today</h2>
+    <div className="barchartToday">
+      <BarChart
+        xAxis={[
+          {
+            id: "barCategories",
+            data: ["Solar", "Wind", "Water"],
+          },
+        ]}
+        series={[
+          {
+            data: [2, 5, 3],
+          },
+        ]}
+        height={300}
+      />
+    </div>
   </>
 );
 
